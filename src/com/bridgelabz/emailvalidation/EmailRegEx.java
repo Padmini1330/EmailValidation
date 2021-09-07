8fda59e83f7e84b1290025062179023d254d7f2a
padmini v <padmini.v@perfios.com>
2021-09-07 20:23:32 +0530

    ADDED: Welcome message in main method

---
+++ src/com/bridgelabz/emailvalidation/EmailRegEx.java
@@ -0,0 +1,12 @@
+package com.bridgelabz.emailvalidation;
+
+public class EmailRegEx 
+{
+
+	public static void main(String[] args) 
+	{
+		System.out.println("**Welcome to Email Validator**");
+
+	}
+
+}
