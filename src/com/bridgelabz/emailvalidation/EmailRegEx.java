8ac38670cc7e20afffa5a34ce962e4e1dc38bded
padmini v <padmini.v@perfios.com>
2021-09-07 20:49:20 +0530

    ADDED: emailValidator method to validate the email address

--- src/com/bridgelabz/emailvalidation/EmailRegEx.java
+++ src/com/bridgelabz/emailvalidation/EmailRegEx.java
@@ -1,11 +1,29 @@
 package com.bridgelabz.emailvalidation;
 
+import java.util.regex.Pattern;
+import java.util.regex.Matcher;
+
 public class EmailRegEx 
 {
 
+	public static boolean emailValidator(String emailAddress) 
+	{
+		Pattern pattern=Pattern.compile("(abc)[a-zA-Z][.@a-zA-Z]*$"); 
+		Matcher matcher=pattern.matcher(emailAddress);
+		return matcher.matches();
+	}
+
 	public static void main(String[] args) 
 	{
-		System.out.println("**Welcome to Email Validator**");
+		System.out.println("**Welcome to Email Evaluator**");
+		
+		String emailAddress="abcd@bridgelabz.co";
+		
+		boolean isValidEmail=emailValidator(emailAddress);
+		if(isValidEmail)
+			System.out.println("Email address "+emailAddress+" is VALID");
+		else
+			System.out.println("Email address "+emailAddress+" is INVALID");
 
 	}
 
